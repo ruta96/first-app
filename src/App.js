f840d612c5e6c86439170d1fdaf3deaf30239507
Michał Ruta <ruciak1996@gmail.com>
2020-11-28 23:53:54 +0100

    building site
    animation on landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import {useRef, useEffect} from 'react';
 import './App.css';
+import { ReactComponent as Scene } from './build.svg';
+import gsap from 'gsap';
 
 function App() {
+  const wrapper = useRef(null);
+
+  useEffect(() => { 
+    const [elements] = wrapper.current.children;
+
+    const window = elements.getElementById('window');
+    const buttons = elements.getElementById('buttons');
+    const icons = elements.getElementById('icons');
+    const man = elements.getElementById('man');
+    const topbar = elements.getElementById('topbar');
+    const womantop = elements.getElementById('womantop');
+    const postimage = elements.getElementById('postimage');
+    const armandchest = elements.getElementById('armandchest');
+    const folder = elements.getElementById('folder');
+
+    gsap.set([window,...buttons.children,...icons.children,man,topbar,womantop,postimage,armandchest,folder], {autoAlpha: 0});
+
+    const tl = gsap.timeline({defaults: {ease: 'power1.inOut'}});
+
+    tl.fromTo(window,{x: '-=300'},{duration: 1, x: '+=300', autoAlpha: 1})
+    .fromTo(topbar,{x: "-=100"}, {duration: 1, x:'+=100', autoAlpha: 1})
+    .fromTo(buttons.children, {scale: '1.5'},{duration: 1, scale: 1, autoAlpha: 1, stagger: 0.2})
+    .fromTo(icons.children, {scale: '1.5'}, {duration: 0.5, scale: 1, autoAlpha: 1, stagger: 0.2})
+    .fromTo(folder, {x: '-=50'}, {duration:1, x: '+=50', autoAlpha: 1})
+    .fromTo([womantop,armandchest,man,postimage], {x: '+=50'}, {duratiom: 1, x: '-=50', autoAlpha: 1}, '-=1')
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={wrapper} className="App">
+      <Scene />
     </div>
   );
 }
