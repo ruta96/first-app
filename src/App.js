1d5035677c822ba8aad3b396453d5be4f31053d4
Michał Ruta <ruciak1996@gmail.com>
2020-11-29 04:19:24 +0100

    some fixes to animation
    -fixed graphic blink in first moment of page loading
    -added text with animation
    -some changes in main graphic animation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import {useRef, useEffect} from 'react';
 import './App.css';
 import { ReactComponent as Scene } from './build.svg';
 import gsap from 'gsap';
+import TextPlugin from "gsap/TextPlugin";
+
+gsap.registerPlugin(TextPlugin);
 
 function App() {
   const wrapper = useRef(null);
@@ -19,7 +22,8 @@ function App() {
     const armandchest = elements.getElementById('armandchest');
     const folder = elements.getElementById('folder');
 
-    gsap.set([window,...buttons.children,...icons.children,man,topbar,womantop,postimage,armandchest,folder], {autoAlpha: 0});
+    gsap.set([window, ...buttons.children, ...icons.children, man, topbar, womantop, postimage, armandchest, folder, ".text"], {autoAlpha: 0});
+    gsap.set(["svg", ".text"], {visibility:"visible"});
 
     const tl = gsap.timeline({defaults: {ease: 'power1.inOut'}});
 
@@ -28,12 +32,15 @@ function App() {
     .fromTo(buttons.children, {scale: '1.5'},{duration: 1, scale: 1, autoAlpha: 1, stagger: 0.2})
     .fromTo(icons.children, {scale: '1.5'}, {duration: 0.5, scale: 1, autoAlpha: 1, stagger: 0.2})
     .fromTo(folder, {x: '-=50'}, {duration:1, x: '+=50', autoAlpha: 1})
-    .fromTo([womantop,armandchest,man,postimage], {x: '+=50'}, {duratiom: 1, x: '-=50', autoAlpha: 1}, '-=1')
+    .fromTo([womantop,armandchest,man,postimage], {x: '+=50'}, {duration: 1, x: '-=50', autoAlpha: 1}, '-=1')
+    .fromTo(postimage, {rotate: -15}, {duration: .5, rotate:0 , autoAlpha: 1})
+    .fromTo(".text",{scale: 1.5, y: '+=100'},{duration: 1, scale: 1, autoAlpha: 1, y: '-=100'}, '-=2')
   }, []);
 
   return (
     <div ref={wrapper} className="App">
       <Scene />
+      <div className='text'>In progress...</div>
     </div>
   );
 }
